59aea4af428aaaee875a05affbb775223bc762e9
Huskitch <huskitch@gmail.com>
2016-03-13 01:11:54 +0000

    Initial Commit

---
+++ OpenNN/OpenNN/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNN
+{
+    public class Class1
+    {
+    }
+}
